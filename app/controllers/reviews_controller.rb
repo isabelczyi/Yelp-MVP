39f7de3ecd2ba11a26b449d39ca79cc044d9cc56
Isabel Cheung <isabelczyi@gmail.com>
2022-02-23 15:12:31 +1000

    refactored with embedded review form in restaurant/show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,16 +9,21 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
+
     respond_to do |format|
       if @review.save
-        format.html { redirect_to restaurant_path(@restaurant), notice: "Review was successfully created." }
+        format.html { redirect_to restaurant_url(@restaurant), notice: "Review was successfully created." }
         format.json { render :show, status: :created, location: @review }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render 'restaurants/show', status: :unprocessable_entity }
         format.json { render json: @review.errors, status: :unprocessable_entity }
       end
     end
+    # if @review.save
+    #   redirect_to restaurant_path(@restaurant)
+    # else
+    #   render 'restaurants/show'
+    # end
   end
 
   private
